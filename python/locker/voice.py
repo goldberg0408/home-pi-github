336549e081fc6d5071448f93bdc3c2de6a3f2458
kimjaejin-desktop <goldberg0408@naver.com>
2017-11-30 16:07:20 +0900

    python

--- python/locker/voice.py
+++ python/locker/voice.py
@@ -4,7 +4,7 @@ import urllib.request
 
 client_id = "h1pO8IUrZsmwiGRqY_dQ"
 client_secret = "fDoPMVSaCA"
-encText = urllib.parse.quote("원하는 사물함을 선택해주세요.")
+encText = urllib.parse.quote("에바쎄바 참치 넙치 꽁치 뭉치면 살고 흩어지면 죽는 각을 부장 판사님은 인정하십니까? 예. 인정합니다")
 data = "speaker=jinho&speed=1&text=" + encText;
 url = "https://openapi.naver.com/v1/voice/tts.bin"
 request = urllib.request.Request(url)
@@ -16,7 +16,7 @@ if(rescode==200):
   print("음성합성완료")
   response_body = response.read()
 
-  with open('choice.mp3', 'wb') as f:
+  with open('agree.mp3', 'wb') as f:
    f.write(response_body)
    #os.system("omxplayer wether.mp3")
 else:
